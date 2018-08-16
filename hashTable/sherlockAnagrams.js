43710524c1e779b9e9b58609f7c300733bfef296
rvalim <ricardo.valim@gmail.com>
2018-08-16 00:16:51 -0300

    sherlockAnagram

--- hashTable/sherlockAnagrams.js
+++ hashTable/sherlockAnagrams.js
@@ -4,14 +4,14 @@ const fs = require('fs');
 
 // process.stdin.resume();
 // process.stdin.setEncoding('utf-8');
-var readStream = fs.createReadStream('D:/Projetos/Hackerrank/hashTable/sherlockAnagrams.txt', 'utf8');
+var readStream = fs.createReadStream('C:/Projetos/Hackerrank/hashTable/sherlockAnagrams.txt', 'utf8');
 let data = ''
 let inputString = '';
 let currentLine = 0;
 
-readStream.on('data', function(chunk) {
+readStream.on('data', function (chunk) {
     data += chunk;
-}).on('end', function() {
+}).on('end', function () {
     inputString = data.replace(/\s*$/, '')
         .split('\n')
         .map(str => str.replace(/\s*$/, ''));
@@ -27,8 +27,53 @@ function readLine() {
 
 // Complete the checkMagazine function below.
 function sherlockAndAnagrams(s) {
+    console.log(s);
+    let arr = s.split('');
+    // let chars = [];
+    let keys = [];
+    let qtde = 0;
 
+    // for (let i = 0; i < arr.length; i++) {
+    //     let p = arr[i];
 
+    //     if (chars[p] === undefined)
+    //         chars[p] = 1
+    //     else {
+    //         chars[p]++;
+    //         qtde++;
+    //     }
+    // }
+
+    // if (!qtde) return qtde;
+
+    for(let i = 0; i < arr.length; i++)
+    {
+        let key = []
+        key.push(arr[i]);
+        qtde += addKey(key, keys);
+
+        for (let j = i + 1; j < arr.length; j ++){
+            key.push(arr[j]);
+            let keyStr = key.sort().join('');
+
+            qtde += addKey(keyStr, keys);
+        }
+    }
+
+    console.log(keys);
+    
+    return qtde;
+}
+
+function addKey(key, keys){
+    if (keys[key] === undefined)
+        keys[key] = 1
+    else {
+        keys[key]++;
+        return 1;
+    }
+
+    return 0;
 }
 
 function main() {
